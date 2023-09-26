cd093b56ea4c8760f205c0e5fd10250e16d70dc4
AlexStan141 <61749830+AlexStan141@users.noreply.github.com>
2023-09-26 10:14:53 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { getTrending } from 'apiFunctions';
 
-async function getMovies() {
+export default async function Home() {
   var movies = await getTrending('day');
-  return movies.results;
-}
-
-export default function Home() {
-  getMovies().then(response => {
-    response.map(movie => {
-      return <p>{movie.title}</p>;
-    });
+  console.log(movies);
+  movies.results.map(movie => {
+    return <p>{movie.title}</p>;
   });
 }
