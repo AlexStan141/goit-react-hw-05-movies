5b7dcf3b4007b696f1bf61f383c2146de953f012
AlexStan141 <61749830+AlexStan141@users.noreply.github.com>
2023-09-28 08:11:34 +0300

    dsdsd

--- src/components/CastItem/CastItem.js
+++ src/components/CastItem/CastItem.js
@@ -6,7 +6,7 @@ export default function CastItem({ actor }) {
   return (
     <div className={css.cast}>
       <img
-        alt=""
+        alt="Image not found"
         src={
           actor.profile_path
             ? 'https://image.tmdb.org/t/p/w500' + actor.profile_path
